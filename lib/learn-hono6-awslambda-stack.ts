0d8b3a27db1b97e23324a4263ba9fb459ab95d74
heiwa4126 <heiwa4126@gmail.com>
2024-11-20 16:08:27 +0900

    update

--- lib/learn-hono6-awslambda-stack.ts
+++ lib/learn-hono6-awslambda-stack.ts
@@ -1,23 +1,23 @@
-import * as cdk from 'aws-cdk-lib'
-import * as apigw from 'aws-cdk-lib/aws-apigateway'
-import * as lambda from 'aws-cdk-lib/aws-lambda'
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
-import { Construct } from 'constructs'
+import * as cdk from "aws-cdk-lib";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import type { Construct } from "constructs";
 
 export class LearnHono6AwslambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props)
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    const fn = new NodejsFunction(this, 'lambda', {
-      entry: 'lambda/index.ts',
-      handler: 'handler',
-      runtime: lambda.Runtime.NODEJS_20_X,
-    })
-    fn.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE,
-    })
-    new apigw.LambdaRestApi(this, 'myapi', {
-      handler: fn,
-    })
-  }
+		const fn = new NodejsFunction(this, "lambda", {
+			entry: "lambda/index.ts",
+			handler: "handler",
+			runtime: lambda.Runtime.NODEJS_20_X,
+		});
+		fn.addFunctionUrl({
+			authType: lambda.FunctionUrlAuthType.NONE,
+		});
+		new apigw.LambdaRestApi(this, "myapi", {
+			handler: fn,
+		});
+	}
 }
