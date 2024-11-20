7b5859df8f2bb3b1427e888fdab3bf865a99f3d0
heiwa4126 <heiwa4126@gmail.com>
2024-11-20 14:29:59 +0900

    initial commit

---
+++ test/learn-hono6-awslambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LearnHono6Awslambda from '../lib/learn-hono6-awslambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/learn-hono6-awslambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LearnHono6Awslambda.LearnHono6AwslambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
